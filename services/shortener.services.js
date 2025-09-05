e01ac552b575c1e17fa5cc508d6531d59fa42436
Ritik Negi <negiritik47@gmail.com>
2025-09-05 18:06:07 +0530

    Zod Validation: Secure Forms, Handle Errors Edit & Update Shortcodes delete Shortcodes

--- services/shortener.services.js
+++ services/shortener.services.js
@@ -8,6 +8,7 @@ export const getAllShortLinks = async (userId) => {
     .from(shortLinksTable)
     .where(eq(shortLinksTable.userId, userId));
 };
+
 export const getShortLinkByShortCode = async (shortCode) => {
   const [result] = await db
     .select()
@@ -16,6 +17,28 @@ export const getShortLinkByShortCode = async (shortCode) => {
   return result;
 };
 
-export const insertShortLink = async ({ url, shortCode }) => {
-  await db.insert(shortLinksTable).values({ url, shortCode });
+export const insertShortLink = async ({ url, shortCode, userId }) => {
+  await db.insert(shortLinksTable).values({ url, shortCode, userId });
+};
+
+// findShortLinkById
+export const findShortLinkById = async (id) => {
+  const [result] = await db
+    .select()
+    .from(shortLinksTable)
+    .where(eq(shortLinksTable.id, id));
+  return result;
+};
+
+// updateShortCode
+export const updateShortCode = async ({ id, url, shortCode }) => {
+  return await db
+    .update(shortLinksTable)
+    .set({ url, shortCode })
+    .where(eq(shortLinksTable.id, id));
+};
+
+// /deleteShortCodeById
+export const deleteShortCodeById = async (id) => {
+  return await db.delete(shortLinksTable).where(eq(shortLinksTable.id, id));
 };
